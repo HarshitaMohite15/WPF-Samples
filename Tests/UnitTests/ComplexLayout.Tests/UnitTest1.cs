734222c277a6a744959fd36aad59803be5d1cf02
Vidyashree M B (WIPRO LIMITED) <v-vmb@microsoft.com>
2025-09-19 09:42:03 +0530

    SampleApp Complex layout - Unit Tests

---
+++ Tests/UnitTests/ComplexLayout.Tests/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using System.IO;
+using NUnit.Framework;
+
+namespace ComplexLayout.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ComplexLayoutExe_LaunchesSuccessfully()
+        {
+            var exePath = Path.GetFullPath(@"ComplexLayout.exe");
+            Assert.That(File.Exists(exePath), $"Executable not found at: {exePath}");
+
+            using (var process = new Process())
+            {
+                process.StartInfo.FileName = exePath;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+
+                process.Start();
+                // Give the process a moment to start
+                Thread.Sleep(1000);
+
+                Assert.That(!process.HasExited, Is.True, "Process exited immediately after launch.");
+
+                // Clean up
+                process.Kill(entireProcessTree: true);
+            }
+        }
+    }
+}
